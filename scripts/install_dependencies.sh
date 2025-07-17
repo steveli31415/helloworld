c03217fcbf9ba5b48811fcfd066583ff3dffc14e
Steve Li <steveli31415@gmail.com>
2025-07-17 00:16:49 -0700

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Install Python and pip if not already present
-sudo yum update -y
-sudo yum install -y python3 python3-pip
+sudo apt update -y
+sudo apt install -y python3 python3-pip
 
 # Install application dependencies
 pip3 install -r /var/www/python-hello-world/requirements.txt
