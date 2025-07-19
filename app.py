96ae832590f1af9787a13e3f8e20343ba946ca03
steveli31415 <steveli31415@gmail.com>
2025-07-18 23:08:52 -0700

    0.0.1 /time

--- app.py
+++ app.py
@@ -4,7 +4,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, AWS!'
+    return 'Hello, AWS! Version 0.0.1'
+
+@app.route('/time')
+def get_time():
+    return {'time': datetime.now().isoformat()}
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
